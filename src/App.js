a4a50473347338311f8c89e564fa8df8df9e7b0a
Aaron Lu <aaronnoh0409@gmail.com>
2022-02-09 21:02:41 -0500

    added test to check if api call is getting the right data from the rest github api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
-
 function App() {
+  const [gitHubName, setGitHubName] = useState('')
+
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/dnsghd49')
+      .then(res => res.json())
+      .then(data => {
+        setGitHubName(data.name)
+      })
+  }, [])
+
   return (
     <div className="App">
       <h1>Github Profile Info:</h1>
+      <h2>{gitHubName}</h2>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,14 @@
+import App from "./App"
+import { render, screen, waitFor } from '@testing-library/react';
 
+beforeEach(() => {
+    // sets everything back to initial state before each test
+    fetch.resetMocks();
+})
 
-
+test("receives GitHub name from GitHub REST API using jest fetch mock", async () => {
+    fetch.mockResponseOnce(JSON.stringify({ name: 'Aaron Lu' }))
+    render(<App />)
+    const gitHubName = await waitFor(() => screen.getByRole('heading', { level: 2 }))
+    expect(gitHubName).toHaveTextContent('Aaron Lu')
+})
